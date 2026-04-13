00a17220c1ca9893d66d3ffec061effa619141dc
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-04-14 00:47:18 +0600

    refactor: remove sidbar components and replace continer div with nav elemets in dahboard nav

--- src/components/layouts/DashboardNavbar.tsx
+++ src/components/layouts/DashboardNavbar.tsx
@@ -1,6 +1,5 @@
 import { AvatarDropdown } from "../ui/DropdownMenuAvatar";
 import { Separator } from "../ui/separator";
-import { SidebarInset, SidebarTrigger } from "../ui/sidebar";
 import DashboardSearch from "./DashboardSearch";
 import { Notifications } from "./Notifications";
 
@@ -17,22 +16,18 @@ export default function DashboardNavbar() {
     },
   ];
   return (
-    <div>
-      <SidebarInset className="border-b">
-        <header className="flex justify-between h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 px-4">
-          <div className="flex items-center gap-2 ">
-            <SidebarTrigger className="-ml-1" />
-          </div>
-          <div>
-            <DashboardSearch />
-          </div>
-          <div className="flex gap-2">
-            <Notifications />
-            <Separator orientation="vertical" />
-            <AvatarDropdown menu={dropdownMenu} />
-          </div>
-        </header>
-      </SidebarInset>
-    </div>
+    <nav className="border-b">
+      <header className="flex justify-between h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12 px-4">
+        <div className="flex items-center gap-2 "></div>
+        <div>
+          <DashboardSearch />
+        </div>
+        <div className="flex gap-2">
+          <Notifications />
+          <Separator orientation="vertical" />
+          <AvatarDropdown menu={dropdownMenu} />
+        </div>
+      </header>
+    </nav>
   );
 }
