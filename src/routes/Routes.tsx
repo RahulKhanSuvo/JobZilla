83a8d659c806bd5c2b364e02934df970687a43aa
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-04-14 10:06:45 +0600

    feat: integrate admin routes into main router and rename admin route object for consistency

--- src/routes/Routes.tsx
+++ src/routes/Routes.tsx
@@ -4,12 +4,14 @@ import { mainRoutes } from "./main.routes";
 import { authRoutes } from "./auth.routes";
 import { candidateRoutes } from "./candidate.routes";
 import { recruiterRoutes } from "./recruiter.routes";
+import { adminRoutes } from "./admin.routes";
 
 const router = createBrowserRouter([
   mainRoutes,
   authRoutes,
   candidateRoutes,
   recruiterRoutes,
+  adminRoutes,
   {
     path: "*",
     element: <Error />,
--- src/routes/admin.routes.tsx
+++ src/routes/admin.routes.tsx
@@ -2,7 +2,7 @@ import AdminLayout from "@/layouts/AdminLayout";
 import AdminDashbaord from "@/pages/Admin/Dashboard/AdminDashbaord";
 import type { RouteObject } from "react-router";
 
-export const adminRoute: RouteObject = {
+export const adminRoutes: RouteObject = {
   path: "/admin",
   element: <AdminLayout />,
   children: [{ path: "/dashbard", element: <AdminDashbaord /> }],
