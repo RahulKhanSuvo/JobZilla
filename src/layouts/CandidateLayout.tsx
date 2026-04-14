d284ee91ed6823ba2801e456db3a261ba00aeb65
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-04-14 20:12:15 +0600

    refactor: remove redundant comments from CandidateLayout component

--- src/layouts/CandidateLayout.tsx
+++ src/layouts/CandidateLayout.tsx
@@ -10,10 +10,8 @@ export default function CandidateLayout() {
       <main className="flex flex-1 overflow-hidden">
         <ScrollRestoration getKey={(location) => location.pathname} />
 
-        {/* Responsive Sidebar (Handles itself internally) */}
         <CandidateSideBar />
 
-        {/* Main Content Area */}
         <div className="flex-1 overflow-y-auto p-4 md:p-6 lg:p-8">
           <div>
             <Outlet />
