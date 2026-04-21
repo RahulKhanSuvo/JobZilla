8aa5366b5d8edeb40a837f444f0b46eb9dada92b
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-04-21 15:35:38 +0600

    feat: add validation to ensure minimum salary does not exceed maximum salary in post job schema

--- src/pages/Recruiter/postjob/postJobSchema.ts
+++ src/pages/Recruiter/postjob/postJobSchema.ts
@@ -17,40 +17,45 @@ export const CareerLevelEnum = z.enum([
   "SENIOR_LEVEL",
   "EXECUTIVE_LEVEL",
 ]);
-export const postJobSchema = z.object({
-  id: z.string().optional(),
-  title: z.string().min(1, "Job title is required"),
-  description: z.string().min(1, "Job description is required"),
-  category: z.string().min(1, "Job category is required"),
-  gender: GenderEnum,
-  salaryType: SalaryTypeEnum,
-  salaryMin: z.coerce.number().min(1, "Salary min is required"),
-  salaryMax: z.coerce.number().min(1, "Salary max is required"),
-  jobType: JobTypeEnum.optional(),
-  locationType: LocationTypeEnum,
-  location: z.string().min(1, "Location is required"),
-  experience: z.string().max(100).min(1, "Experience is required"),
-  careerLevel: CareerLevelEnum,
-  qualification: z.string().min(1, "Qualification is required"),
-  deadline: z.coerce.date().refine((date) => date > new Date(), {
-    message: "Deadline must be a future date",
-  }),
-  createdAt: z.date().optional(),
-  updatedAt: z.date().optional(),
-  isSaved: z.boolean().optional(),
-  isApplied: z.boolean().optional(),
-  isFollowed: z.boolean().optional(),
-  user: z
-    .object({
-      id: z.string(),
-      name: z.string(),
-      email: z.string().email(),
-      company: z.object({
-        location: z.string(),
-        logo: z.string(),
-      }),
-    })
-    .optional(),
-});
+export const postJobSchema = z
+  .object({
+    id: z.string().optional(),
+    title: z.string().min(1, "Job title is required"),
+    description: z.string().min(1, "Job description is required"),
+    category: z.string().min(1, "Job category is required"),
+    gender: GenderEnum,
+    salaryType: SalaryTypeEnum,
+    salaryMin: z.coerce.number().min(1, "Salary min is required"),
+    salaryMax: z.coerce.number().min(1, "Salary max is required"),
+    jobType: JobTypeEnum.optional(),
+    locationType: LocationTypeEnum,
+    location: z.string().min(1, "Location is required"),
+    experience: z.string().max(100).min(1, "Experience is required"),
+    careerLevel: CareerLevelEnum,
+    qualification: z.string().min(1, "Qualification is required"),
+    deadline: z.coerce.date().refine((date) => date > new Date(), {
+      message: "Deadline must be a future date",
+    }),
+    createdAt: z.date().optional(),
+    updatedAt: z.date().optional(),
+    isSaved: z.boolean().optional(),
+    isApplied: z.boolean().optional(),
+    isFollowed: z.boolean().optional(),
+    user: z
+      .object({
+        id: z.string(),
+        name: z.string(),
+        email: z.string().email(),
+        company: z.object({
+          location: z.string(),
+          logo: z.string(),
+        }),
+      })
+      .optional(),
+  })
+  .refine((data) => data.salaryMin <= data.salaryMax, {
+    message: "Minimum salary cannot be greater than maximum salary",
+    path: ["salaryMin"],
+  });
 
 export type PostJobFormData = z.infer<typeof postJobSchema>;
