22198516636690d8de2b8c8b144b13c63d7feee7
Rahul Khan <rahul.khan.suvo@gmail.com>
2026-04-22 18:27:45 +0600

    feat: enable rich colors for toast notifications in main entry point

--- src/main.tsx
+++ src/main.tsx
@@ -11,7 +11,7 @@ createRoot(document.getElementById("root")!).render(
     <ReduxProvider>
       <ThemeProvider>
         <RouterProvider router={router} />
-        <Toaster />
+        <Toaster richColors />
       </ThemeProvider>
     </ReduxProvider>
   </StrictMode>,
